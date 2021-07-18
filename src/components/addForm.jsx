b12bf2b2ea50192c416f714cd3b9417ae0e99408
hajin <hajin@synapsoft.co.kr>
2021-07-18 16:44:17 +0900

    chapter/4_17 addForm을 함수형 컴포넌트로 변경

--- src/components/addForm.jsx
+++ src/components/addForm.jsx
@@ -1,28 +1,27 @@
-import React, { PureComponent } from 'react';
 
-class AddForm extends PureComponent {
-    formRef = React.createRef();
-    inputRef = React.createRef();
+import React, { memo } from 'react';
 
-    onSubmit = event => {
+const AddForm = memo((props) => {
+    const formRef = React.createRef();
+    const inputRef = React.createRef();
+
+    const onSubmit = (event) => {
         event.preventDefault();
-        const name = this.inputRef.current.value;
-        name && this.props.onAdd(name);
-        this.formRef.current.reset();
-    };
-    render() {
-        return (
-            <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-                <input
-                    ref={this.inputRef}
-                    type="text"
-                    className="add-input"
-                    placeholder="Habit"
-                />
-                <button className="add-button">Add</button>
-            </form>
-        );
+        const name = inputRef.current.value;
+        name && props.onAdd(name);
+        formRef.current.reset();
     }
-}
+    return (
+        <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+            <input
+                ref={inputRef}
+                type="text"
+                className="add-input"
+                placeholder="Habit"
+            />
+            <button className="add-button">Add</button>
+        </form>
+    );
+});
 
 export default AddForm;
